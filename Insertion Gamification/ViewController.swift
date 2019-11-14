9c737012526608966aa0aa9f21f14ecb033d74ae
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-11-13 20:24:43 -0500

    Initial Commit

---
+++ Insertion Gamification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Insertion Gamification
+//
+//  Created by Kyle Gil Tan on 11/13/19.
+//  Copyright © 2019 Kyle Gil Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
