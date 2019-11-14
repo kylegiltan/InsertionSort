613ad78ded8db63c10b8482ce3887caa4d040bbb
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-11-13 20:36:47 -0500

    Move left move right

--- Insertion Gamification/ViewController.swift
+++ Insertion Gamification/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //have array
+    
+    
+    @IBAction func moveLeftPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, animations: {self.imageImage.frame.origin.x = self.imageImage.frame.origin.x-20})
+    }
+    
+    @IBAction func moveRightPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, animations: {self.imageImage.frame.origin.x = self.imageImage.frame.origin.x+20})
+    }
+    
 
 }
 
