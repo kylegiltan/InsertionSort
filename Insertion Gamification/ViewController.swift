7f176c422916292f28b8097d385312560b8e340b
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-11-21 19:47:56 -0500

    messing with coordinates

--- Insertion Gamification/ViewController.swift
+++ Insertion Gamification/ViewController.swift
@@ -33,29 +33,22 @@ class ViewController: UIViewController {
         var i = 0
         
         UIView.animate(withDuration: 3, animations: {
-            self.box1.frame.origin.y = self.box1.frame.origin.y-378
-            self.one.frame.origin.y = self.one.frame.origin.y-378
-            self.box1.frame.origin.x = self.box1.frame.origin.x+77
-            self.one.frame.origin.x = self.one.frame.origin.x+77
+            self.box1.frame.origin.y = 160
+            self.one.frame.origin.y = 160
+            self.box1.frame.origin.x = 190
+            self.one.frame.origin.x = 190
         })
-        
 
         UIView.animate(withDuration: 2, animations: {
-            self.box3.frame.origin.x = self.box3.frame.origin.x-20
-            self.three.frame.origin.x = self.three.frame.origin.x-20})
-        
-        UIView.animate(withDuration: 2, animations: {
-            self.box2.frame.origin.x = self.box2.frame.origin.x-49
-            self.two.frame.origin.x = self.two.frame.origin.x-49})
-        
-        
+            self.box2.frame.origin.x = 115
+            self.two.frame.origin.x = 115
+            self.box3.frame.origin.x = 266
+            self.three.frame.origin.x = 266})
         
         while(i < 17){
-            
             UIView.animate(withDuration: 10, animations: {
                 self.box1.frame.origin.y = self.box1.frame.origin.y+20
                 self.one.frame.origin.y = self.one.frame.origin.y+20})
-            
             i += 1
         }
         
@@ -72,14 +65,21 @@ class ViewController: UIViewController {
     
     //MARK:- Movements
     @IBAction func moveLeftPressed(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.5, animations: {self.box1.frame.origin.x = self.box1.frame.origin.x-20
+        if self.box1.frame.origin.x <= 10{}
+        else{
+        UIView.animate(withDuration: 0.5, animations: {
+            self.box1.frame.origin.x = self.box1.frame.origin.x-20
             self.one.frame.origin.x = self.one.frame.origin.x-20
         })
+        }
     }
     
     @IBAction func moveRightPressed(_ sender: UIButton) {
+        if self.box1.frame.origin.x >= 370 {}
+        else{
         UIView.animate(withDuration: 0.5, animations: {self.box1.frame.origin.x = self.box1.frame.origin.x+20
             self.one.frame.origin.x = self.one.frame.origin.x+20})
+        }
     }
     
     //MARK:- Start
@@ -95,27 +95,30 @@ class ViewController: UIViewController {
         three.text = "\(threeint)"
         four.text = "\(fourint)"
         zero.text = "\(zeroint)"
-        self.box0.frame.origin.y = 541
-        self.zero.frame.origin.y = 541
-        self.box0.frame.origin.x = 20
-        self.zero.frame.origin.x = 20
-        self.box1.frame.origin.x = 95
-        self.one.frame.origin.x = 95
-        self.box1.frame.origin.y = 541
-        self.one.frame.origin.y = 541
-        self.box3.frame.origin.x = 247
-        self.three.frame.origin.x = 247
-        self.box3.frame.origin.y = 541
-        self.three.frame.origin.y = 541
-        self.box2.frame.origin.x = 171
-        self.two.frame.origin.x = 171
-        self.box2.frame.origin.y = 541
-        self.two.frame.origin.y = 541
+        self.box0.frame.origin.x = 40
+        self.zero.frame.origin.x = 40
+        self.box0.frame.origin.y = 540
+        self.zero.frame.origin.y = 540
+        self.box1.frame.origin.x = 115
+        self.one.frame.origin.x = 115
+        self.box1.frame.origin.y = 540
+        self.one.frame.origin.y = 540
+        self.box2.frame.origin.x = 190
+        self.two.frame.origin.x = 190
+        self.box2.frame.origin.y = 540
+        self.two.frame.origin.y = 540
+        self.box3.frame.origin.x = 265
+        self.three.frame.origin.x = 265
+        self.box3.frame.origin.y = 540
+        self.three.frame.origin.y = 540
+        self.box4.frame.origin.x = 340
+        self.four.frame.origin.x = 340
+        self.box4.frame.origin.y = 540
+        self.four.frame.origin.y = 540
         newGame(first: oneint, second: zeroint)
     }
     
     
-    //MARK:- ViewController
     //MARK:- Insert
     @IBAction func insertButtonPressed(_ sender: UIButton) {
         
